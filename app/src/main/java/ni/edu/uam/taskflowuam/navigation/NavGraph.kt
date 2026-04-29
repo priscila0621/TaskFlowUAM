0b539bfa1f4fbf19f84f856185b9dffa367adfed
jeyni02 <jeynilomary@uam.edu.ni>
2026-04-28 21:25:18 -0600

    feat(navigation): implementar rutas y navegación entre pantallas

--- app/src/main/java/ni/edu/uam/taskflowuam/navigation/NavGraph.kt
+++ app/src/main/java/ni/edu/uam/taskflowuam/navigation/NavGraph.kt
@@ -1,2 +1,35 @@
-package ni.edu.uam.taskflowuam.navigation
+package ni.edu.uam.taskflow.navigation
 
+import androidx.compose.animation.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.*
+import ni.edu.uam.taskflow.ui.screens.*
+import ni.edu.uam.taskflow.viewmodel.TaskViewModel
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun NavGraph(vm: TaskViewModel) {
+
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "home") {
+
+        composable("home") {
+            HomeScreen(navController, vm)
+        }
+
+        composable("add") {
+            AddTaskScreen(navController, vm)
+        }
+
+        composable("detail/{id}") {
+            val id = it.arguments?.getString("id")?.toInt() ?: 0
+            DetailScreen(navController, vm, id)
+        }
+
+        composable("edit/{id}") {
+            val id = it.arguments?.getString("id")?.toInt() ?: 0
+            EditTaskScreen(navController, vm, id)
+        }
+    }
+}
