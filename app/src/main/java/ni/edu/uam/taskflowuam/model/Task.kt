e150885b2f98abde71830c70cc069da8ca6fc4e1
jeyni02 <jeynilomary@uam.edu.ni>
2026-04-28 21:23:19 -0600

    feat(model): crear clase Task para representar tareas

--- app/src/main/java/ni/edu/uam/taskflowuam/model/Task.kt
+++ app/src/main/java/ni/edu/uam/taskflowuam/model/Task.kt
@@ -1,2 +1,22 @@
-package ni.edu.uam.taskflowuam.model
+package ni.edu.uam.taskflow.model
 
+enum class Priority {
+    HIGH, MEDIUM, LOW;
+
+    fun displayName(): String {
+        return when (this) {
+            HIGH -> "Alta"
+            MEDIUM -> "Media"
+            LOW -> "Baja"
+        }
+    }
+}
+
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean = false,
+    val priority: Priority = Priority.MEDIUM,
+    val dueDate: Long? = null
+)
